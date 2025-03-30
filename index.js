38b708b7eac8682ffb33f83585d80b9f17a61117
SwatiPaiGhatkar <swatipaighatkar@gmail.com>
2025-03-30 14:08:12 +1100

    added java script to display current temp via API

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+function displayWeather(response) {
+  let temperatureElement = document.querySelector("#temp");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let currentCityElement = document.querySelector("#current-city");
+  currentCityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "0d020a074f787obb6e00f25733t5d32d";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
 function handleSearchData(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let currentCityElement = document.querySelector("#current-city");
-  currentCityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
